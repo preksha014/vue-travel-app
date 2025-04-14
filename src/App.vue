55877ebafa866e1fb72729e4448cf33a847f5b66
Preksha <ranapreksha8@gmail.com>
2025-04-14 10:37:07 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

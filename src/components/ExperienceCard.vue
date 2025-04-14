2bcb04520f5c72774951cd23de96fe58c393c577
Preksha <ranapreksha8@gmail.com>
2025-04-14 15:51:32 +0530

    Added Experience Cards

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <img :src="`/images/${experience.image}`" :alt="experience.name"/>
+        <span class="card__text">
+            {{ experience.name }}
+        </span>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    experience: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -6,26 +6,37 @@
             <p>{{ destination.description }}</p>
         </div>
     </section>
+    <section class="experiences">
+        <h2>Top Experiences in {{ destination.name }}</h2>
+        <div class="cards">
+            <router-link v-for="experience in destination.experiences" :key="experience.slug"
+                :to="{ name: 'experience.show', params: { experienceSlug: experience.slug } }">
+                <ExperienceCard :experience="experience" />
+            </router-link>
+        </div>
+
+    </section>
 </template>
 
 <script setup>
 import sourceData from '../data.json'
-import { computed,defineProps } from 'vue'
+import { computed, defineProps } from 'vue'
+import ExperienceCard from '@/components/ExperienceCard.vue'
 // import { useRoute } from 'vue-router'
 
 // const route = useRoute()
 // const destination = ref(null)
 const props = defineProps({
-  id: {
-    type: Number,
-    required: true
-  }
+    id: {
+        type: Number,
+        required: true
+    }
 })
 
 //const destinationId = computed(() => parseInt(route.params.id))
 const destination = computed(() => {
     return sourceData.destinations.find(
-        destination => destination.id ===props.id
+        destination => destination.id === props.id
     )
 })
 // const fetchDestination = async () => {
